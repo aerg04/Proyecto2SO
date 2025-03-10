75a0b0d79ffec11e62b41924fba3634bb7f076b8
Andres Rangel <andres.rangel@correo.unimet.edu.ve>
2025-03-10 14:47:13 -0400

    creación proyecto

---
+++ src/main/java/proyecto2/proyecto2/Proyecto2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto2.proyecto2;
+
+/**
+ *
+ * @author DELL
+ */
+public class Proyecto2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
